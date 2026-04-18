da414e95580c6aa3f136ee4eeae93a25e3eda9aa
Simran Sandhu <snsandhu@ucdavis.edu>
2026-04-17 17:58:03 -0700

    min_index

--- src/sorting.cpp
+++ src/sorting.cpp
@@ -49,7 +49,7 @@ int min_index_of_array(int* ar, int len) {
   int min_index = 0;
 
   for (int i = 1; i < len; ++i) {
-    if (ar[i] > ar[min_index]) {
+    if (ar[i] < ar[min_index]) {
       min_index = i;
     }
   }
