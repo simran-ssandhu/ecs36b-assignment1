2860578b0fde04d3768690f6a7c5dea831c7b587
Simran Sandhu <snsandhu@ucdavis.edu>
2026-04-17 19:59:41 -0700

    getsortedtests - simplesortreversesortedarray

--- testing/test_get_sorted.cpp
+++ testing/test_get_sorted.cpp
@@ -32,6 +32,19 @@ TEST(GetSortedTests, SimpleSortReverseSortedArray) {
      * Don't forget to free any memory that was dynamically allocated as part of your test.
      */
 
+  int* arr = new int[5]{5, 4, 3, 2, 1};
+
+    int * sortedArray = get_sorted(arr, 5);
+
+  EXPECT_EQ(sortedArray[0], 1);
+  EXPECT_EQ(sortedArray[1], 2);
+  EXPECT_EQ(sortedArray[2], 3);
+  EXPECT_EQ(sortedArray[3], 4);
+  EXPECT_EQ(sortedArray[4], 5);
+
+  delete[] arr;
+    delete[] sortedArray;
+
 }
 
 TEST(GetSortedTests, SimpleSortAverageArray) {
